725a0d163e316d19db9e1fb14e423b290e46c4cc
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-12 08:22:59 +0430

    Function store on IPFS done!

--- src/App.js
+++ src/App.js
@@ -1,30 +1,47 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import ipfs from "ipfs";
+// import ipfs from "ipfs";
+
+const ipfsClient = require("ipfs-http-client");
+// Getting CORS error on this client
+// const ipfs = ipfsClient("http://localhost:5001");
+// Temp using infura for dev
+const ipfs = ipfsClient("https://ipfs.infura.io:5001");
 
 function App() {
-  const [resourceHash, setResourceHash] = useState("hash comes here");
+  const [resourceHash, setResourceHash] = useState("");
   const [loading, setLoading] = useState(false);
+  const [loadingMsg, setLoadingMsg] = useState("Msg comes here..");
   const [fileBuffer, setFileBuffer] = useState(null);
 
-  //Gunction to submit file to IPFS
-  const submitToIPFS = (e) => {
+  //Function to submit file to IPFS
+  const submitToIPFS = async (e) => {
     e.preventDefault();
     setLoading(true);
+    setLoadingMsg("File is being uploaded to IPFS");
+
+    for await (const result of ipfs.add(fileBuffer)) {
+      setResourceHash(result.path);
+      setLoading(false);
+      setLoadingMsg("Data stored succesfully on IPFS with above hash");
+    }
   };
 
   //Function to read the file into a buffer
-  const readFile = async (e) => {
+  const readFile = (e) => {
     e.preventDefault();
     setLoading(true);
+    setLoadingMsg("File is being read to a buffer");
+
     const file = e.target.files[0];
-    console.log(file);
+    // console.log(file);
     const bufferdReader = new window.FileReader();
     bufferdReader.readAsArrayBuffer(file);
     bufferdReader.onloadend = () => {
       setFileBuffer(Buffer(bufferdReader.result));
       setLoading(false);
+      setLoadingMsg("File read complete");
     };
   };
 
@@ -43,13 +60,15 @@ function App() {
         <input type="submit" />
       </form>
       <p>{resourceHash}</p>
-      <p>{fileBuffer}</p>
       <hr />
       <input type="text" placeholder="File hash to get form IPFS" />
       <button type="button" onClick={getFileIPFS}>
         Get file
       </button>
-      <div>{loading && <span>Loading...</span>}</div>
+      <div>
+        {loading && <span>Loading...</span>}
+        <p>{loadingMsg}</p>
+      </div>
     </div>
   );
 }
--- src/ipfs.js
+++ src/ipfs.js
@@ -1,5 +1,7 @@
-import ipfsHttpClient from "ipfs-http-client";
+// import ipfsHttpClient from "ipfs-http-client";
 
-const ipfs = ipfsHttpClient("http://127.0.0.1:5001/");
+// const ipfs = ipfsHttpClient("http://127.0.0.1:5001/");
 
+const ipfsApi = require("ipfs-api");
+const ipfs = new ipfsApi("localhost", "5001", { protocol: "http" });
 export default ipfs;
