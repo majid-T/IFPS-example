7378826ee7944ac30badecbf4c6e779d48a5d931
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-12 06:12:14 +0430

    Added skeleton component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ipfs from "ipfs";
 
 function App() {
+  const [resourceHash, setResourceHash] = useState("sss");
+  const [loading, setLoading] = useState(false);
+
+  const submitToIPFS = (e) => {
+    e.preventDefault();
+    setLoading(true);
+  };
+  const readFile = (e) => {
+    e.preventDefault();
+  };
+  const getFileIPFS = (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>IPFS File Upload</h1>
+      <h2>
+        For demostration purposes this application will work with images only.
+      </h2>
+      <form onSubmit={submitToIPFS}>
+        <input type="file" onChange={readFile} />
+        <input type="submit" />
+      </form>
+      <p>{resourceHash}</p>
+      <hr />
+      <input type="text" placeholder="File hash to get form IPFS" />
+      <button type="button" onClick={getFileIPFS}>
+        Get file
+      </button>
+      <div>{loading && <span>Loading...</span>}</div>
     </div>
   );
 }
