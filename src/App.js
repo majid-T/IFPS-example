57768e445933ac8eb55540206fccb3328faaf12e
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-06-12 06:31:35 +0430

    function file reader done

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import "./App.css";
 import ipfs from "ipfs";
 
 function App() {
-  const [resourceHash, setResourceHash] = useState("sss");
+  const [resourceHash, setResourceHash] = useState("hash comes here");
   const [loading, setLoading] = useState(false);
+  const [fileBuffer, setFileBuffer] = useState(null);
 
+  //Gunction to submit file to IPFS
   const submitToIPFS = (e) => {
     e.preventDefault();
     setLoading(true);
   };
-  const readFile = (e) => {
+
+  //Function to read the file into a buffer
+  const readFile = async (e) => {
     e.preventDefault();
+    setLoading(true);
+    const file = e.target.files[0];
+    console.log(file);
+    const bufferdReader = new window.FileReader();
+    bufferdReader.readAsArrayBuffer(file);
+    bufferdReader.onloadend = () => {
+      setFileBuffer(Buffer(bufferdReader.result));
+      setLoading(false);
+    };
   };
+
+  //Function to get the file from IPFS by hash code
   const getFileIPFS = (e) => {
     e.preventDefault();
 
@@ -23,14 +38,12 @@ function App() {
   return (
     <div>
       <h1>IPFS File Upload</h1>
-      <h2>
-        For demostration purposes this application will work with images only.
-      </h2>
       <form onSubmit={submitToIPFS}>
         <input type="file" onChange={readFile} />
         <input type="submit" />
       </form>
       <p>{resourceHash}</p>
+      <p>{fileBuffer}</p>
       <hr />
       <input type="text" placeholder="File hash to get form IPFS" />
       <button type="button" onClick={getFileIPFS}>
